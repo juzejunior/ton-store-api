ddd941dfddaca798a6f74e80585d53477aa93f55
José <josejuniordsc@gmail.com>
2021-04-22 14:13:37 -0300

    feat: added uuid to generate product id

---
+++ index.js
@@ -0,0 +1,43 @@
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+const { v4: uuidv4 } = require('uuid');
+
+exports.handler = async (event, context) => {
+   
+   const headers = {
+    "Content-Type": "application/json"
+  };
+
+  const tableName = "product";
+  
+  let body;
+  let statusCode = 200;
+  
+  try {
+      let requestJSON = JSON.parse(event.body);
+      let product = { 
+         id: uuidv4(),
+         price: requestJSON.price,
+         name: requestJSON.name,
+      }
+      await dynamo
+          .put({
+            TableName: tableName,
+            Item: product
+          }).promise();
+      body = product;    
+  } catch(err) {
+      statusCode = 400;
+      body = err.message;        
+  } finally {
+      body = JSON.stringify(body);
+  }  
+   
+  const response = {
+        statusCode: statusCode,
+        body: body,
+        headers: headers
+  };
+  
+  return response;
+};
